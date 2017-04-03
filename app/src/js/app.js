e0125ba6f3a3f95e8bdb2e3897f6c9da71cad6a5
BraveLilToaster <penguindude2691@gmail.com>
2017-04-03 14:41:05 -0400

    finsihed the service page, need to update pics and copy for clothing and fabrics pages

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,20 +1,7 @@
 (function () {
   var vm = this;
 
-  $(document).ready(function(){
-    $('.hero-slider').slick({
-      accessibility: true,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      cssEase: 'ease',
-      easing: 'linear',
-      infinite: true,
-      pauseOnHover: true,
-      speed: 300
-
-    });
-  });
+  // Toggle Nav Menu
   var hideMenu = function() {
     if ( document.getElementById("navigation").className.match(/(?:^|\s)hide-menu(?!\S)/) ) {
       document.getElementById("navigation").className =
@@ -27,47 +14,31 @@
       console.info('menu open');
     }
   }
-  var scrollTo = function(link){
-    console.info("scrolling to "+ link);
-    $('html, body').animate({scrollTop: $(link).offset().top -100 }, 'slow');
-  }
 
-  var initializeParallax = function(){
-    if ($(window).width() < 600 && vm.parallaxOn) {
-      turnOffParallax();
-    }
-    else if ($(window).width() >= 600 && !vm.parallaxOn){
-      turnOnParallax();
-    }
-  }
-  var turnOnParallax = function(){
-      console.log("Turn on parallax")
-      vm.parallaxOn = true;
-      vm.pwindow = new Rellax('.parallax-window',{
-        center: true,
-        speed: -6,
-        percentage: .5
-      });
-      vm.pslider = new Rellax('.parallax-slider',{
-        center: true,
-        speed: -1,
-        percentage: .9
-      });
-  }
-  var turnOffParallax = function() {
-      console.log("Turn off parallax")
-      vm.parallaxOn = false;
-      vm.target = document.querySelectorAll('.parallax-window');
-      Array.prototype.forEach.call(target, function(element){
-        element.removeAttribute('style');
-      });
-      vm.pwindow.destroy();
-      vm.target = document.querySelectorAll('.parallax-slider');
-      Array.prototype.forEach.call(target, function(element){
-        element.removeAttribute('style');
-      });
-      vm.pslider.destroy();
-  }
+  // Homepage Slider
+  $(document).ready(function(){
+    $('.hero-slider').slick({
+      accessibility: true,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      cssEase: 'ease',
+      easing: 'linear',
+      infinite: true,
+      pauseOnHover: true,
+      speed: 300
+    });
+  });
+
+  // Parallax
+  $('.parallax-window').parallax({
+    naturalWidth: 600,
+    naturalHeight: 400,
+    iosFix: true,
+    androidFix: true
+  });
+
+// Smooth Scrolling
   smoothScroll.init({
     selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
     selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
@@ -76,10 +47,7 @@
     offset: 80, // Integer. How far to offset the scrolling anchor location in pixels
     callback: function ( anchor, toggle ) {} // Function to run after scrolling
   });
-  $(window).resize(function() {
-    initializeParallax();
-  });
-  initializeParallax();
+
   window.onload = function() {
     document.getElementById("menu-icon").addEventListener( 'click' , hideMenu );
   }
