a19326a6625cb737b375d021499c6df20973eead
BraveLilToaster <penguindude2691@gmail.com>
2017-06-14 15:06:59 -0400

    added modal for newsletter. still need to make the functions more generic and change the event that opens the modal

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -48,6 +48,27 @@
     callback: function ( anchor, toggle ) {} // Function to run after scrolling
   });
 
+  // Modal
+  var modal = document.getElementById('modal');
+
+  var btn = document.getElementById("myBtn");
+
+  var span = document.getElementsByClassName("modal-close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
   window.onload = function() {
     document.getElementById("menu-icon").addEventListener( 'click' , hideMenu );
   }
